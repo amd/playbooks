8defbecb2d5826537190b30b15f22afe2c0085d4
Holanda Noronha <dholanda@amd.com>
2026-01-11 07:28:51 -0800

    Fix font

--- website/src/app/layout.tsx
+++ website/src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import localFont from 'next/font/local';
 
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
+const inter = localFont({
+  src: './font/local/inter.woff2',
+  weight: '400',
+  style: 'normal',
 });
+
 
 export const metadata: Metadata = {
   title: "STX Halo™ | AMD",
